f67b40d26f760e56a0c8e9eaf1aa20965bccfeb3
Darbyblinko <jacobcrombie@live.com>
2020-07-28 22:28:34 -0600

    contacts-list final

---
+++ main.js
@@ -0,0 +1,68 @@
+let contacts = []
+
+function addContact(event) {
+  event.preventDefault()
+  let form = event.target
+
+  let contact = {
+    id: generateId(),
+    name: form.name.value,
+    phone: form.phone.value,
+    emergencyContact: form.emergencyContact.checked
+  }
+  contacts.push(contact)
+  saveContacts()
+  form.reset()
+}
+
+function saveContacts() {
+  window.localStorage.setItem("contacts", JSON.stringify(contacts))
+  drawContacts()
+}
+
+function loadContacts() {
+  let contactsData = JSON.parse(window.localStorage.getItem("contacts"))
+  if (contactsData) {
+    contacts = contactsData
+  }
+  drawContacts()
+}
+
+function drawContacts() {
+  let template = ""
+
+  contacts.forEach(contact => {
+    template += `
+    <div class="card mt-1 mb-1 ${contact.emergencyContact ? `emergency-contact` : ''}">
+      <h3 class="mt-1 mb-1">${contact.name}</h3>
+      <div class="d-flex space-between">
+      <p>
+      <i class="fa fa-fw fa-phone"></i>
+      <span>${contact.phone}</span>
+      </p>
+      <i type="button" class="action fa fa-trash text-danger" onclick="removeContact('${contact.id}')"></i>
+      </div>
+    </div>
+    `
+  })
+  document.getElementById("contact-list").innerHTML = template
+}
+
+function removeContact(contactId) {
+  let remIndex = contacts.findIndex(contact => contact.id == contactId)
+  contacts.splice(remIndex,1)
+  saveContacts()
+}
+
+function toggleAddContactForm() {
+  document.getElementById("new-contact-form").classList.toggle("hidden")
+}
+
+
+function generateId() {
+  return Math.floor(Math.random() * 10000000) + "-" + Math.floor(Math.random() * 10000000)
+}
+
+
+loadContacts()
+drawContacts()
